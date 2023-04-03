a7ede58eb22f9e7ee8246250503c70789777d8dd
AidasD <ntaidas@gmail.com>
2023-04-03 23:43:26 +0100

    cannot prevent default

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-unneeded-ternary */
 // array with books inside that is displayed
 const myLibrary = [];
 const board = document.getElementById('display');
 
 // Form Elements
-const startForm = document.getElementById('big_plus');
-const add = document.getElementById('add');
-const bookName = document.querySelector('#book_name').value;
-const author = document.getElementById('author').value;
-const pages = document.getElementById('pages').value;
-const read = document.getElementById('if_read').value;
-const putBook = document.getElementById('put_book').value;
+const bigPlus = document.getElementById('big_plus');
+const addBook = document.getElementById('add_book');
+const formCard = document.getElementById('add_to_library');
 
 // Toggles between button and form
 function addForm() {
-  startForm.style.display = 'none';
-  add.style.display = 'grid';
+  bigPlus.style.display = 'none';
+  formCard.style.display = 'grid';
 }
 // Book constructor
+bigPlus.addEventListener('click', addForm);
 
 function Book(title, author, pages, read = Boolean) {
   this.title = title;
@@ -34,41 +34,58 @@ myLibrary.push(book2);
 
 // populates display with books
 function populate() {
-  for (const book of myLibrary) {
-  // create card elements
+  const container = document.querySelector('#board');
+  for (let book of myLibrary) {
+    // create card element
     const card = document.createElement('div');
-    card.setAttribute('class', 'card');
+    card.classList.add('card');
     card.innerHTML = `
-    <p>${book.title}</p>
-    <p>${book.author}</p>
-    <p>${book.pages}</p>
-    <div class="${read}"></div>
+      <p>Title: ${book.title}</p>
+      <p>Author: ${book.author}</p>
+      <p>Pages: ${book.pages}</p>
+      <div class="read-status${book.read ? ' read' : ''}"></div>
     `;
     const position = myLibrary.indexOf(book);
-    console.log(position);
-    // assign titles to elements
-    if (this.read === true) {
-      isRead.setAttribute('id', 'true');
-    }
-
-    // add elements
-    display.appendChild(card);
-    card.style.order = `${position + 1}`;
+    // assign order to card
+    card.style.order = `${-position}`;
+    // add card to container
+    container.appendChild(card);
   }
 }
 
-function addBook(bookName, author, pages, read) {
-  console.log(author);
-  const book = new Book(`${bookName}`, `${author}`, `${pages}`, `${read}`);
+function addCardToBoard(book) {
+  // create card element
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.innerHTML = `
+    <p>Title: ${book.title}</p>
+    <p>Author: ${book.author}</p>
+    <p>Pages: ${book.pages}</p>
+    <div class="read-status${book.read ? ' read' : ''}"></div>
+  `;
+  const position = myLibrary.indexOf(book);
+  // assign order to card
+  card.style.order = `${-position}`;
+  // add card to container
+  board.appendChild(card);
+}
+
+addBook.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(addBook);
+  const title = formData.get('title');
+  const author = formData.get('author');
+  const pages = formData.get('pages');
+  const read = formData.get('if_read');
+  const book = new Book(title, author, pages, read);
   myLibrary.push(book);
+  document.getElementById('add_to_library').reset();
+  // add new card to board
+  addCardToBoard(book);
+  // clear and repopulate the board
+  populate();
+}, false);
+
+window.onload = function() {
   populate();
 }
-
-function addBookToLibrary(input) {
-  myLibrary.push(input);
-}
-
-startForm.addEventListener('click', addForm);
-
-addBook('papas', 'lapas', 10, true);
-putBook.addEventListener('click', addBook);
